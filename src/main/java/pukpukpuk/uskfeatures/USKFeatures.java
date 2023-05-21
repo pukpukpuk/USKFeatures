ca8bb6b869788cf95242f6b2d9932546926c8da9
pukpukpuk <pukpukpuk228@proton.me>
2023-05-21 11:00:20 +0500

    Init commit

---
+++ src/main/java/pukpukpuk/uskfeatures/USKFeatures.java
@@ -0,0 +1,16 @@
+package pukpukpuk.uskfeatures;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class USKFeatures extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
